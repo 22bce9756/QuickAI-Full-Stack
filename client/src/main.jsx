0e69f68bd1575497084f523ef721317f7a6e6fe8
Navya Kethavath <kethavathsandhyasree@gmail.com>
2025-08-24 01:33:28 +0530

    Added ClerkProvider with custom frontendApi and updated .env

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,23 +1,25 @@
-
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import { ClerkProvider } from '@clerk/clerk-react'
 
-
-// Import your Publishable Key
+// Import your Publishable Key & Frontend API
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+const FRONTEND_API = import.meta.env.VITE_CLERK_FRONTEND_API
 
 if (!PUBLISHABLE_KEY) {
   throw new Error('Missing Publishable Key')
 }
 
 createRoot(document.getElementById('root')).render(
-  <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl='/'>
+  <ClerkProvider 
+    publishableKey={PUBLISHABLE_KEY} 
+    frontendApi={FRONTEND_API} 
+    afterSignOutUrl="/"
+  >
     <BrowserRouter>
-       <App />
-    </BrowserRouter>,
+      <App />
+    </BrowserRouter>
   </ClerkProvider>
-  
 )
