83e86d9309dcd8f904209543ea783319585ac182
Navya Kethavath <kethavathsandhyasree@gmail.com>
2025-08-24 11:53:13 +0530

    Configured Clerk keys and updated env files

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,23 +1,20 @@
-
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import { ClerkProvider } from '@clerk/clerk-react'
 
-
-// Import your Publishable Key
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+const FRONTEND_API = import.meta.env.VITE_CLERK_FRONTEND_API
 
 if (!PUBLISHABLE_KEY) {
-  throw new Error('Missing Publishable Key')
+  throw new Error('Missing Clerk Publishable Key')
 }
 
 createRoot(document.getElementById('root')).render(
-  <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl='/'>
+  <ClerkProvider publishableKey={PUBLISHABLE_KEY} frontendApi={FRONTEND_API} afterSignOutUrl="/">
     <BrowserRouter>
-       <App />
-    </BrowserRouter>,
+      <App />
+    </BrowserRouter>
   </ClerkProvider>
-  
 )
