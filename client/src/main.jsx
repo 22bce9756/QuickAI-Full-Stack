36c0f0483ca239c6a015d947cfced71e31082a12
Navya sree Kethavath <kethavathnavyasree@gmail.com>
2025-08-31 15:04:57 +0530

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,20 +1,24 @@
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
-import { ClerkProvider } from '@clerk/clerk-react'
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+import { BrowserRouter } from 'react-router-dom';
+import { ClerkProvider } from '@clerk/clerk-react';
 
-const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
-const FRONTEND_API = import.meta.env.VITE_CLERK_FRONTEND_API
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+const FRONTEND_API = import.meta.env.VITE_CLERK_FRONTEND_API;
 
 if (!PUBLISHABLE_KEY) {
-  throw new Error('Missing Clerk Publishable Key')
+  throw new Error('Missing Clerk Publishable Key');
 }
 
 createRoot(document.getElementById('root')).render(
-  <ClerkProvider publishableKey={PUBLISHABLE_KEY} frontendApi={FRONTEND_API} afterSignOutUrl="/">
+  <ClerkProvider
+    publishableKey={PUBLISHABLE_KEY}
+    frontendApi={FRONTEND_API}
+    afterSignOutUrl="/"
+  >
     <BrowserRouter>
       <App />
     </BrowserRouter>
   </ClerkProvider>
-)
+);
